70c1072c8ae6cf301fb6c2ccd938472476450a54
jan-far <ligalifarouk@gmail.com>
2020-06-10 16:52:21 +0100

    Update employee_controller.js

--- employee_controller.js
+++ employee_controller.js
@@ -1,13 +1,14 @@
 import db from "../../db/create-db";
 
 //getting all Employee data
-exports.AllEmployee = (req,res)=>{
+exports.AllEmployee = (req, res) => {
     const department_id = parseInt(request.params.department_id)
-db.query('SELECT * FROM employees WHERE department_id=$1', [department_id], 
-(error, results) => {
-        if (err) {
-            res.status(400).send({"status":"error", "data": err });
-        }
-        res.status(200).send({"status":"success", "data":results})
-    })
+
+    db.query('SELECT * FROM employees WHERE department_id=$1', [department_id],
+        (error, results) => {
+            if (err) {
+                res.status(400).send({ "status": "error", "data": err });
+            }
+            res.status(200).send({ "status": "success", "data": results })
+        })
 };
