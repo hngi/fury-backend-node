8cce3b7b553f491034c0bdaec01da6bed1f536fd
courage173 <courageosemwengie@gmail.com>
2020-06-10 18:52:53 +0100

    feature[created update emplayee schema for validation]

--- src/api/utils/validationRules.js
+++ src/api/utils/validationRules.js
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { parseAsync } from '@babel/core';
 
 export const createEmployeeSchema = {
     body: Joi.object().keys({
@@ -12,4 +13,20 @@ export const createEmployeeSchema = {
         address: Joi.string().min(5).required()
     })
 }
+
+export const updateEmployeeSchema = {
+    body: Joi.object().keys({
+        firstName: Joi.string().min(2).max(100).required(),
+        lastName: Joi.string().min(2).max(100).required(),
+        email: Joi.string().email().required(),
+        hireDate: Joi.date().required(),
+        departmentId: Joi.number().required(),
+        phoneNo: Joi.string().regex(/^\d{3}-\d{3}-\d{4}$/).required(),
+        address: Joi.string().min(5).required()
+    }),
+    params: Joi.object().keys({
+        employeeId: Joi.number().required()
+    })
+}
+
 
