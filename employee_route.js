ad6db9507f12c6920e9cbb2469c962fd8af07cf1
jan-far <ligalifarouk@gmail.com>
2020-06-10 16:47:27 +0100

    Update employee_route.js

--- employee_route.js
+++ employee_route.js
@@ -1,10 +1,7 @@
 const controller = require("../controllers/employee_controller");
 const router = require("express").Router();
 
-// Create new  Employee details
-router.post("/employees", controller.createEmployee)
-
-//Updating Employee detail
-router.put("/employee/employeeId", controller.updateEmployee)
+// All Employee details
+router.post("/employees", controller.AllEmployee)
 
 module.exports = router;
