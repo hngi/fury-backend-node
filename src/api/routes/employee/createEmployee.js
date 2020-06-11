b0e17c52454e77cb778e666c87787fca0d320ee9
Edwin Kayang <pelpuo2000@gmail.com>
2020-06-11 03:24:12 +0000

    Create employees
    Removed helper functions

--- src/api/routes/employee/createEmployee.js
+++ src/api/routes/employee/createEmployee.js
@@ -1,22 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const createDB = require("./../../db/create-db"); 
-const helpers = require('./helpers');
 
 router.post('/', async (req,res) =>{
-    /* Fields = firstName(req), lastName(req), phoneNo(req), 
-    email(req), departmentId(req), hireDate(req), address, userId(req)
-*/
     const {firstName, lastName, phoneNo, email, departmentId, hireDate, address} = req.body
 
     if(address == null)address = "";
-
-    //Checking for empty or invalid inputs
-    if(helpers.isEmpty(firstName) && helpers.isEmpty(lastName)
-    && helpers.empty(phoneNo) && helpers.isValidEmail(email)
-    && helpers.empty(departmentId) && helpers.empty(hireDate) ){
-        return res.status(400).json({status: "Error", data: "Invalid arguments for fields"})
-    }
     try{
         const query = 'INSERT INTO eployees(first_name, last_name, phone_no, email, department_id, hire_date, address) values($1, $2, $3, $4, $5, $6, $7)';
         const vars = [firstName, lastName, phoneNo, email, departmentId, hireDate, address]
@@ -26,10 +15,12 @@ router.post('/', async (req,res) =>{
             if (error) {
                 throw error
               }
-            response.status(200).json({message:`User added with ID: ${result.insertId}`})
+            response.status(200).json({message:`User added with ID: ${results.insertId}`})
         });
     }catch(e){
         res.status(400).json({status: "Error", data: "Invalid arguments for fields"})
     }
 
 });
+
+module.exports = router;
