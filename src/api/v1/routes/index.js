b44b8f78008fea537297972edff183e5a3966808
Dave <callremz@gmail.com>
2020-06-12 15:35:51 +0100

    feature: add response handler

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { swaggerSpec } from "../../utils/swaggerSpec";
+import responseHandler from "../../utils/responseHandler";
 
 const router = express.Router();
 
 // route for documentation in json
 router.get("/documentation", (req, res) => {
   res.status(200).json(swaggerSpec);
+});
+
+// route for configuration
+router.get("/configuration", (req, res) => {
+  responseHandler(res, 200, {
+    message: "configuration received",
+  });
 });
 
 export default router;
