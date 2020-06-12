8a48ca4a76b37f8804507d78a72dedac464966b2
Dave <callremz@gmail.com>
2020-06-12 01:27:16 +0100

    wip: change swagger dir and url

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { swaggerSpec } from "../../utils/swagger-spec";
 
 const router = express.Router();
 
-// default response to base URL
-router.get("/", (req, res) => {
-  res.status(200).send("Employee records microservice is up and running!");
+// documentation in json
+router.get("/documentation", (req, res) => {
+  res.status(200).json(swaggerSpec);
 });
 
 export default router;
