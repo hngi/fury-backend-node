3ab183c32c19c2dd9b5b8214802e0195079bda4a
Dave <callremz@gmail.com>
2020-06-13 03:25:22 +0100

    fix: few tweaks

--- src/api/db/sql/db-create.sql
+++ src/api/db/sql/db-create.sql
@@ -1,12 +1,12 @@
 -- SQL DDL statements go here
 
 -- This is an internal table for migration
-CREATE TABLE public.DB_VERSION (
+CREATE TABLE IF NOT EXISTS public.DB_VERSION (
     VERSION numeric NOT NULL,
     ID smallint NOT NULL
 );
 
-CREATE SEQUENCE public.DB_VERSION_ID_seq
+CREATE SEQUENCE IF NOT EXISTS public.DB_VERSION_ID_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -16,16 +16,19 @@ CREATE SEQUENCE public.DB_VERSION_ID_seq
 ALTER TABLE ONLY public.DB_VERSION ALTER COLUMN ID SET DEFAULT nextval('public.DB_VERSION_ID_seq'::regclass);
 
 ALTER TABLE ONLY public.DB_VERSION
+    DROP CONSTRAINT IF EXISTS DB_VERSION_pkey;
+
+ALTER TABLE ONLY public.DB_VERSION
     ADD CONSTRAINT DB_VERSION_pkey PRIMARY KEY (ID);
 
-CREATE TABLE departments (
+CREATE TABLE IF NOT EXISTS departments (
      id bigserial NOT NULL PRIMARY KEY,
      name character varying,
      manager_id BIGINT,
      user_id BIGINT
 );
 
-CREATE TABLE employees (
+CREATE TABLE employees IF NOT EXISTS (
        id bigserial NOT NULL PRIMARY KEY,
        first_name character varying,
        last_name character varying,
@@ -36,3 +39,10 @@ CREATE TABLE employees (
        address character varying,
        user_id BIGINT
 );
+
+CREATE TABLE migrations IF NOT EXISTS (
+    id serial NOT NULL PRIMARY KEY,
+    migration_filename character varying,
+    db_version smallint
+    date_created TIMESTAMP
+)
