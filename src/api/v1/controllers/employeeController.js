f9f8311d830b969199f4e2a31d941aa87f8faba2
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-13 12:54:07 +0200

    feat: add deleteEmployee controller

--- src/api/v1/controllers/employeeController.js
+++ src/api/v1/controllers/employeeController.js
@@ -1,30 +1,69 @@
-import db from '../../db';
-import responseHandler from '../../utils/responseHandler';
-import CustomError from '../../utils/customError';
+import db from "../../db";
+import responseHandler from "../../utils/responseHandler";
+import CustomError from "../../utils/customError";
 
 export const createEmployee = async (req, res, next) => {
-    try {
-        const { userId } = req.user
-        const { firstName, lastName, phoneNo
-            , email, departmentId,
-            hireDate, address } = req.body
-        const body = [firstName, lastName, phoneNo, email, departmentId, hireDate, address, userId]
-        const sql = `
+  try {
+    const { userId } = req.user;
+    const {
+      firstName,
+      lastName,
+      phoneNo,
+      email,
+      departmentId,
+      hireDate,
+      address,
+    } = req.body;
+    const body = [
+      firstName,
+      lastName,
+      phoneNo,
+      email,
+      departmentId,
+      hireDate,
+      address,
+      userId,
+    ];
+    const sql = `
             INSERT INTO employees (first_name, last_name,phone_no, email, department_id,hire_date,address,user_id)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING *
           `;
-        let rows = await db.query(sql, body)
+    let rows = await db.query(sql, body);
 
-        return responseHandler(res, 201, {
-            data: rows[0].id
-        })
+    return responseHandler(res, 201, {
+      data: rows[0].id,
+    });
+  } catch (error) {
+    const err = new CustomError(400, {
+      status: "error",
+      data: error.message,
+    });
+    next(err);
+  }
+};
 
-    } catch (error) {
-        const err = new CustomError(400, {
-            status: 'error',
-            data: error.message
-        })
-        next(err)
+export const deleteEmployee = async (req, res, next) => {
+  try {
+    const { employeeId } = req.params;
+
+    const employee = await db.query(
+      `SELECT * FROM employees WHERE id=${employeeId}`
+    );
+    if (!employee[0]) {
+      const err = new CustomError(400, "employee does not exist");
+      return next(err);
     }
-}
+    await db.query(`DELETE FROM employee WHERE id=${employeeId}`);
+    return responseHandler(res, 204, {
+      data: `${employee[0].first_name} ${employee[0].last_name} deleted successfully`,
+    });
+  } catch (error) {
+    console.log(error);
+    const err = new CustomError(400, {
+      status: "error",
+      data: error.message,
+    });
+    next(err);
+  }
+};
